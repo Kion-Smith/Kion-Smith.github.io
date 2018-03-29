f67f0fd46e9d4ac54c9844ad784eaaf914ebe09b
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-03-28 23:15:11 -0500

    adding message box

--- form.js
+++ form.js
@@ -1,6 +1,7 @@
 function myFunction() 
 {
-	var x = document.getElementById("myText").value;
-	alert("Hi "+x)
-    ;
+	var email = document.getElementById("userEmail").value;
+	var mesg = document.getElementById("Message").value
+	
+	alert("Your email is"+ email +" and your message is :: "+ mesg);
 }
