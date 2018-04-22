747e3eaeb4d17d10a32273199667a43c1fa978f3
Kion-Smith <kion_smith@hotmail.com>
2018-04-21 21:39:20 -0500

    temp

--- form.js
+++ form.js
@@ -7,17 +7,3 @@ function myFunction()
 	alert("Your email is "+ email +" and your subject is "+ sub +" with the message :: "+ mesg );
 }
 
-function tempAnimation() {
-  var elem = document.getElementById("myAnimation");   
-  var pos = 0;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      elem.style.top = pos + 'px'; 
-      elem.style.left = pos + 'px'; 
-    }
-  }
-}
