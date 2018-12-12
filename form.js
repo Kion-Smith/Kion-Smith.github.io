504c294b2375ea05b1a877dffc30493a901032ec
Kion-Smith <kion_smith@hotmail.com>
2018-12-11 22:41:00 -0600

    Porting over to new website design

--- form.js
+++
@@ -1,9 +0,0 @@
-function myFunction() 
-{
-	var email = document.getElementById("userEmail").value;
-	var mesg = document.getElementById("Message").value
-	var sub = document.getElementById("userSubject").value;
-	
-	alert("Your email is "+ email +" and your subject is "+ sub +" with the message :: "+ mesg );
-}
-
